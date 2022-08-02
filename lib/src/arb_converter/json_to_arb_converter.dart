c7d60a8fb5f57b78b67d01148f2266264cce8538
Omar H <98951974+ohabash-sabbar@users.noreply.github.com>
2022-08-02 16:27:53 +0400

    MOD process arb keys to our liking
    - skip keys beginning with dot
    - make first letter camel case
    - replace all dots with underscore

--- lib/src/arb_converter/json_to_arb_converter.dart
+++ lib/src/arb_converter/json_to_arb_converter.dart
@@ -11,7 +11,24 @@ class JsonToArbConverter {
           accumulator..addFlattenedEntry(entry),
     );
     arbMap.addAll(flattenedKeyMap);
-    return arbMap;
+
+    final Map<String, dynamic> processedArbMap = {};
+    arbMap.forEach((key, value) {
+      // 1. Skips
+      // 1.1 skip any entry when its key begins with a dot
+      if (key[0] == '.') {
+        return;
+      }
+
+      // 2. Transformations
+      // 2.1 Make first letter lower case (camel case)
+      String newKey = key[0].toLowerCase() + key.substring(1);
+      // 2.2 Replace all dots with underscore
+      newKey = newKey.replaceAll('.', '');
+
+      processedArbMap[newKey] = value;
+    });
+    return processedArbMap;
   }
 }
 
